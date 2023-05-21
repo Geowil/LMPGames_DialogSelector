7f46a3f6c1f27f7ee7096ab1f6c4e1fec8a4b791
Geowil <geowil11@gmail.com>
2023-05-21 04:31:47 -0700

    initial plugin files

---
+++ LMPGames_DialogSelector.js
@@ -0,0 +1,247 @@
+/*:
+* @plugindesc V0.5 | Allows for configuration of switch-based dialog system.
+* @author LMPGames
+*
+*
+* @param Settings
+* @desc Contains all of the main plugin settings
+*
+*
+* @param Enable Dialog System
+* @desc When enabled, turns on the dialog system
+* @type boolean
+* @default false
+* @on Enable
+* @off Disable
+* @parent Settings
+*
+*
+* @param Text Game Variable
+* @desc Assigns a game variable to be used to store dialog text
+* @type variable
+* @default 1
+* @parent Settings
+*
+*
+* @param Dialog Settings
+* @desc Contains all of the dialog settings
+*
+*
+* @param Map Data
+* @desc A map containing npc dialog data per map
+* @type struct<MapData>[]
+* @default []
+* @parent Dialog Settings
+*
+*
+* @param Default Dialog
+* @desc The dialog returned when an invalid value is used in the GetDialog plugin command
+* @type text
+* @default Hello, \P[1]
+* @parent Dialog Settings
+*
+*
+* @param Unconfigured NPC Dialog
+* @desc The dialog returned when an invlaid map or npc is used with the system.
+* @type text
+* @default <Wordwrap>This map or npc has not been configured in the Dialog Selector plugin configuration data.\nTest new line
+* @parent Dialog Settings
+*
+*
+* @help
+* For more information on how to use this plugin, please see the
+* GitHub page:
+*
+* https://github.com/Geowil/LMP_DialogSelector
+*/
+
+/*~struct~MapData:
+* @param Map Id
+* @desc Stores the ID of the map this data is related to
+* @type number
+* @min 1
+* @default 1
+*
+*
+* @param Npc List
+* @desc Stores npc data for npcs on the current map
+* @type struct<NpcData>[]
+* @default []
+*/
+
+/*~struct~NpcData:
+* @param Npc Id
+* @desc Stores the ID of the npc this data is related to
+* @type number
+* @min 1
+* @default 1
+*
+*
+* @param Dialog List
+* @desc List of dialog data configuration objects
+* @type struct<DialogData>[]
+* @default []
+*/
+
+/*~struct~DialogData:
+* @param Conditions
+* @desc List of switches that when true, will allow the dialog to be displayed
+* @type switch[]
+* @default []
+*
+*
+* @param Dialog
+* @desc Dialog text to be displayed
+* @type text
+* @default ""
+*/
+
+var lmpgames_DialogSelectorParams = PluginManager.parameters("LMPGames_DialogSelector");
+var enableDialogSystem = (lmpgames_DialogSelectorParams["Enable Dialog System"] == "true");
+var textVariableId = parseInt(lmpgames_DialogSelectorParams["Text Game Variable"]);
+var mapData = JSON.parse(lmpgames_DialogSelectorParams["Map Data"]);
+var defaultDialog = lmpgames_DialogSelectorParams["Default Dialog"];
+var unconfigDialog = lmpgames_DialogSelectorParams["Unconfigured NPC Dialog"];
+
+var parsedMapData = [];
+var mapDataHasBeenParsed = false;
+
+/* Data Manager Functions */
+var lmpgamesDialogSelectorDatabaseManager_IsMapLoaded = DataManager.isMapLoaded;
+DataManager.isMapLoaded = function() {
+	let mapIsLoaded = lmpgamesDialogSelectorDatabaseManager_IsMapLoaded.call(this);
+	if (mapIsLoaded) {
+		if (!mapDataHasBeenParsed) {
+			processMapData();
+			mapDataHasBeenParsed = true;
+			$gameSystem.toggleDialogSystem(enableDialogSystem);
+		}
+	}
+	return mapIsLoaded;
+};
+
+/* Plugin Commands */
+var lmpgamesDialogSelectorGameInterpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+Game_Interpreter.prototype.pluginCommand = function(command, args){
+	if (command === "LMPGames.DialogSelector") {
+		for (let arg of args) {
+			command += " " + arg;
+		}
+
+		let matches = [];
+		if (command.match(/LMPGames.DialogSelector[ ]EnableDialogSystem[ ](\d+)/)) {
+			matches = ((/LMPGames.DialogSelector[ ]EnableDialogSystem[ ](\d+)/).exec(command) || []);
+			if (matches.length > 1) {
+				$gameSystem.toggleDialogSystem(matches[1]);
+			}
+		} else if (command.match(/LMPGames.DialogSelector[ ]GetDialog[ ](\d+)[ ](\d+)/)) {
+			matches = ((/LMPGames.DialogSelector[ ]GetDialog[ ](\d+)[ ](\d)/).exec(command) || []);
+			if (matches.length > 2) {
+				$gameSystem.setDialogVariable(matches[1], matches[2]);
+			}
+		}
+	} else {
+		lmpgamesDialogSelectorGameInterpreter_pluginCommand.call(this, command, args);
+	}
+}
+
+
+/* Game System Functions */
+Game_System.prototype.toggleDialogSystem = function(dialogSystemEnabled){
+	this.bDialogSystemEnabled = dialogSystemEnabled;
+}
+
+Game_System.prototype.isDialogSystemEnabled = function(){
+	return this.bDialogSystemEnabled == true;
+}
+
+Game_System.prototype.setDialogVariable = function(mapId, npcId){
+	let paramIsInvalid = false;
+	if (mapId && mapId > 0 &&
+		$gameMap._mapId == mapId)
+	{
+		let map = parsedMapData.find(map => map && map["Map Id"] == mapId);
+		if (map) {
+			if (npcId && npcId > 0) {
+				let events = $gameMap._events;
+				let npcs = map["Npc List"];
+
+				let event = events.find(event => event && event._eventId == npcId);
+				let npc = npcs.find(npc => npc && npc["Npc Id"] == npcId);
+				if (event && npc) {
+					let dialogWasSelected = false;
+					let dialogObjectList = JSON.parse(JSON.stringify(npc["Dialog List"]));
+					dialogObjectList.reverse();
+					for (let dialogObject of dialogObjectList) {
+						let conditionList = dialogObject.Conditions;
+						conditionList = conditionList.map(ele => {
+							return Number(ele);
+						});
+
+						let gameSwitches = $gameSwitches._data;
+						let conditions = gameSwitches.filter((swt, idx) => conditionList.includes(idx));
+						if (conditionList.length == conditions.length) {
+							let trueConditions = conditions.filter(swt => swt === true);
+							if (trueConditions.length == conditions.length) {
+								$gameVariables.setValue(textVariableId, dialogObject.Dialog);
+								dialogWasSelected = true;
+								break;
+							}
+						}
+					}
+
+					if (!dialogWasSelected) {
+						$gameVariables.setValue(textVariableId, defaultDialog);
+					}
+				} else {
+					paramIsInvalid = true;
+				}
+			} else {
+				paramIsInvalid = true;
+			}
+		} else {
+			paramIsInvalid = true;
+		}
+	} else {
+		paramIsInvalid = true;
+	}
+
+	if (paramIsInvalid) {
+		$gameVariables.setValue(textVariableId, unconfigDialog);
+	}
+}
+
+/* Window_Message Functions & Aliases */
+var lmpgamesDialogSelector_WindowBase_ConvertEscapeCharacters = Window_Base.prototype.convertEscapeCharacters;
+Window_Base.prototype.convertEscapeCharacters = function(text) {
+	this._wasVariable = text.match(/\\V\[(\d+)\]/gi);
+	text = lmpgamesDialogSelector_WindowBase_ConvertEscapeCharacters.call(this, text);
+    if (this._wasVariable) {
+		text = this.convertEscapeCharacters(text);
+	}
+
+	return text;
+};
+
+/* Utility Functions */
+function processMapData(){
+	for (let data of mapData) {
+		parsedMapData.push(parseMapData(data));
+	}
+}
+
+function parseMapData(dataString){
+	let data = JSON.parse(dataString);
+	for (let key in data) {
+		let value = data[key];
+		if (value.substr(0, 2) == '[\"' ||
+			value.substr(0, 2) == '[]' ||
+			value.substr(0, 2) == '{\"' ||
+			value.substr(0, 2) == '{}')
+		{
+			data[key] = parseMapData(value);
+		}
+	}
+
+	return data;
+}
---
+++ readme.md
@@ -0,0 +1,143 @@
+# Dialog Selector
+
+The Dialog Selector plugin allows you to create a rudimentary branching dialog system using a plugin setting for configuration of npc dialog per map.  Dialog can also be set up to requiire certain swtiches to be active in order for that dialog to be shown.  When used in combination with some eventing, you can create a basic dialog system.
+
+
+Plugin Version: 0.5
+
+Plugin Demo(http://www.lmpgames.com/plugins/demos/LMPGames_DialogSelector_Demo_V0.5.zip)
+
+Plugin Project(http://www.lmpgames.com/plugins/projects/LMPGames_DialogSelector_Project_V0.5.zip)
+
+
+## Installing the plugin
+Download the plugin file either using the Demo or Project links above or from the repository.  Please the .js file into your js/Plugins folder and then add the plugin into your project in the Plugin Manager screen.
+
+Note, YanFly_MessageCore is highly recommended but optional.
+
+
+## Plugin Information
+This section contains information relating to plugin settings and commands used with plugin.  To learn how to use and what certain commands or settings do, continue reading.  If you are looking for configuraiton information, see the Configation Settings section below.
+
+
+### Plugin Settings
+
+#### Enable Dialog System
+This setting controls whether or not the dialog system is enabled when the game starts.  Set this to true to enable the system at the start of the game.
+Boolean
+true
+
+
+#### Text Game Variable
+This setting sets the game variable that will be used to store the current npc dialog to display.
+**Required**
+Game Variable
+001
+
+
+#### Map Data
+This setting contains the configuration for each map and each npc on those maps that the Dialog System should apply to.  For more information on how to configure this setting, see the Configuration Settings section below.
+Required
+Object
+
+
+#### Default Dialog
+This setting sets the dialog shown by an NPC when there is no valid dialog available to be shown.  This only applies to configured NPCs.  Please note that the ESC character is used; do not change this.
+Text
+Hello, \P[1]
+
+
+#### Unconfigured NPC Dialog
+This setting sets the dialog to be displayed when calling the plugin command from an NPC that either has not figured for the current map or whose map is not configured in the Map Data plugin setting.  This is meant to be used as a development debugging tool, an option to turn this output off or on will be added in a future version.
+Text
+This map or npc has not been configured in the Dialog Selector plugin configuration data.
+
+
+
+### Plugin Commands
+#### Tutorial.DialogSelector GetDialog <MapId> <NpcId>
+This plugin command allows you to call the GetDialog function to get the current valid dialog for an NPC on a specific map.  Note that Map Id must be the id of the current map the NPC event is currently on or the system will show the text set in the Unconfigured NPC Dialog setting.
+
+**Parameters**
+- MapId
+  - Sets the ID for the map that the current NPC is on.  Must match the ID for the currently map.
+- NpcId
+  - Sets the NPC ID that plugin should look for to find the proper dialog configuration for.
+
+**Example**
+```
+LMPGames.DialogSelector GetDialog 1 2
+```
+
+
+#### LMPGames.DialogSelector EnableDialogSystem <0/1>
+This plugin command allows you to enable or disable the dialog system from an NPC within the game.  0 means that the system is disabled, while 1 means that the system is enabled.
+
+**Parameters**
+- <0/1>
+  - 0 = Disabled / 1 = Enabled
+
+**Example**
+```
+LMPGames.DialogSelector EnableDialogSystem 0
+```
+
+
+
+### Configuration Settings
+This section will detail how to configure the Map Data plugin setting for use with the plugin.
+
+#### Map Configuration
+The first level of configuiration is to set up the configuration records for each map that will contain NPCs that are a part of the dialog system.  If an NPC does not use the GetDialog plugin command, it does not need to be configured here and if no NPCs on a map use it either then the map doesn't need to be configured here either.
+
+The first step is to identify the Map ID for the map you're configuring.  You can find this, easily, by looking at the bottom of the editor window.  In the footer bar on the right side you should see some values, of which the left most one is your map id and name.  Use this map id for the Map Id setting.
+
+Next you'll need to set up the NPC configration settings for each NPC on the map that will use the system.  Double click on the Npc List setting.
+
+
+#### NPC Configuration
+This is the second level of configuration for the plugin.  Here you will, like you did with the Map Configuration, locate the NPC Id for the NPC you are going to enter dialog configuraiton for and place that ID into the Npc Id setting.  This can be found in the NPC event window in the editor in the top left corner of the event window.
+
+Once you have this, you'll need to configure the dialog settings for this specific npc.  Double click on the Dialog List setting.
+
+
+#### Dialog Configuration
+This is the final level of configuration for the plugin.  To configure dialog for use by the NPC create new rows by double clicking in an empty space in the list.  You should then see a window with the Conditions and Dialog settings.
+
+Dialog is the text that the NPC should use when the conditions are met.
+
+Conditions is the list of switches that should be turned on in order to the NPC to use this specific dialog.  If there should be no conditions, such as for setting up default text, then leave the Conditions setting set to the default value.
+
+When adding Conditions, you will be asked to select the Switch from a drop down.  It is advisable to separate your dialog switches from your other switches and to name them at this point so that they can easiy be found and understood later when setting up any additional event confgiration you may require.
+
+##### Text Codes
+Text codes are supported in dialog text configured within the plugin.
+
+
+**For more specifics on how this data can be configured, see the plugin project for examples**
+
+
+
+## Planned Future Enhancements
+- Ability to turn on or off YanFly_MessageCore support
+- Ability to turn on or off Unconfigured NPC Dialog setting
+
+
+## Possible Enhancements
+None at this time
+
+
+## Conflicts
+Any plugin that overrides or aliases Window_Base.convertEscapeCharacters may conflict with this plugin without integration/a patch.
+
+
+## Change Log
+- Version 0.5
+  - New Features
+    - Support for text codes within the dialog configuration.
+	- Confirmed that MessageCore functionality works from dialog configuration.
+
+- Version 0.1
+  - Initial version of plugin
+
+
